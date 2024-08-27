90b16e87b48f5d9d1f893348fcb3437dbd3d384e
Alex Mwaura <alecsmwaura@gmail.com>
2024-08-27 18:19:39 +0300

    updates

--- js/animation.js
+++ js/animation.js
@@ -1,25 +1,16 @@
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach((entry) =>{
-      console.log(entry)
-      if (entry.isIntersecting){
-        entry.target.classList.add('show');
+// Controls the footer wave animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
       }
-      else
-      {
-        entry.target.classList.remove('show');
-      }
-    });
   });
+});
 
-  const hiddenElements = document.querySelectorAll('.animate, .animate2');
-  hiddenElements.forEach((el) => observer.observe(el));
+const hiddenElements = document.querySelectorAll('.animate, .animate2');
+hiddenElements.forEach((el) => observer.observe(el));
 
-  window.addEventListener('load', function () {
-const preloader = document.querySelector('.preloader');
-setTimeout(function () {
-  preloader.style.opacity = '0';
-  setTimeout(function () {
-    preloader.style.display = 'none';
-  }, 500); // Adjust the delay as needed
-}, 1000); // Adjust the delay as needed
-}); 
+// The preloader-related code has been removed
